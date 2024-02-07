fd3ee097a75bf68d5e355c5a0a0136512ce9a23f
Abiranjan <abiranjan.p@gmail.com>
2024-02-07 14:34:26 +0100

    CF-10 middleware route configured

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import LoginLayout from './(pages)/(auth)/layout'
-import Login from './(pages)/(auth)/login/page'
-
 export default function Page() {
-  return (
-    <LoginLayout>
-      <Login></Login>
-    </LoginLayout>
-  )
+  return <div></div>
 }
---
+++ src/middleware.ts
@@ -0,0 +1,39 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+const authRoutes = ['/login', '/signup']
+const protectedRoutes = ['/tournaments', '/dashboard']
+
+export function middleware(request: NextRequest) {
+  const pathName = request.nextUrl.pathname
+  const totalRoutes = [...authRoutes, ...protectedRoutes].find(route =>
+    pathName.includes(route),
+  )
+  if (!totalRoutes) {
+    return redirectRoute(request, authRoutes[0])
+  }
+}
+
+const redirectRoute = (request: NextRequest, pathName: string) => {
+  const url = request.nextUrl.clone()
+  url.pathname = pathName
+  return NextResponse.redirect(url)
+}
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    {
+      source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+      missing: [
+        { type: 'header', key: 'next-router-prefetch' },
+        { type: 'header', key: 'purpose', value: 'prefetch' },
+      ],
+    },
+  ],
+}
