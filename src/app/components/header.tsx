f88ef5ec915a8e11bcc92a89fa3e14c988309334
Abiranjan <abiranjan.p@gmail.com>
2024-02-08 23:54:11 +0100

    CF-10 added header as common layout

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,3 +1,34 @@
+'use client'
+
+import { Avatar } from '@mui/material'
+import Brand from './brand'
+import { COLORS } from '@/util/colors'
+import { useSessionStorage } from '@/hooks/useSessionStorage'
+import { useEffect, useState } from 'react'
+import { User } from '@/model/entities/user.interface'
+
 export default function Header() {
-  return <div>Header</div>
+  const [userData, setUserData] = useState<User | null>()
+  const { getItem } = useSessionStorage()
+
+  useEffect(() => {
+    const data: User | null = getItem('user')
+    console.log(data)
+    setUserData(data)
+  }, [])
+
+  return (
+    <div className='h-14 pl-2 pr-10 white-bg flex justify-between items-center shadow-md'>
+      <Brand />
+      <Avatar
+        sx={{
+          bgcolor: COLORS.cricPrimary,
+          fontSize: 15,
+          textTransform: 'capitalize',
+        }}
+      >
+        {userData?.name && userData?.name[0]}
+      </Avatar>
+    </div>
+  )
 }
