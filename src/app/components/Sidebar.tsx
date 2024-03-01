8d90a7428e83eea5d3d9a0e789dd3e771f461237
Abiranjan <abiranjan.p@gmail.com>
2024-03-01 02:04:35 +0100

    CF-13 brand renamed back to upper case

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import List from '@mui/material/List'
 import CricListItem from './ui/CricListItem'
-import Brand from './brand'
+import Brand from './Brand'
 import { usePathname } from 'next/navigation'
 import { sidebarConfig } from '@/(pages)/tournaments/layout'
 
