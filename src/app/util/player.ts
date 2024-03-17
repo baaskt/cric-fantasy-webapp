a3a5bc3f7adac614b768730f7ae45c02b3d61a2d
Abiranjan <abiranjan.p@gmail.com>
2024-03-17 02:12:52 +0100

    CF-15 ppreserve original categories on grouping

--- src/app/util/player.ts
+++ src/app/util/player.ts
@@ -5,22 +5,38 @@ import { currencyToString } from './bidding'
 import { SquadEntity } from '@/model/entities/squad.interface'
 import { groupListByProp } from './helper'
 
+const ALLROUNDER = 'All Rounder'
+const BAT_ALLROUNDER = 'Batting Allrounder'
+const BOWL_ALLROUNDER = 'Bowling Allrounder'
+
+const WK = 'Wicket Keeper'
+const BAT_WK = 'WK-Batter'
+const BOWL_WK = 'WK-Bowler'
 export const getPlayerDisplayRole = (role: string, squadCount: number): string => {
   let displayRole = role
-  if (role === 'Bowling Allrounder' || role === 'Batting Allrounder') {
-    displayRole = 'All Rounder'
-  } else if (role === 'WK-Batter' || role === 'WK-Bowler') {
-    displayRole = 'Wicket Keeper'
+  if (role === BOWL_ALLROUNDER || role === BAT_ALLROUNDER) {
+    displayRole = ALLROUNDER
+  } else if (role === BAT_WK || role === BOWL_WK) {
+    displayRole = WK
   }
   return squadCount > 1 ? `${displayRole}s` : displayRole
 }
 
 export const groupPlayersByRole = (squad: SquadEntity[]): Map<string, SquadEntity[]> => {
-  const tempSquad = [...squad]
-  tempSquad.forEach(player => {
-    player.role = getPlayerDisplayRole(player.role, 0)
-  })
-  const tempGroupedSquad = groupListByProp<SquadEntity>('role', tempSquad)
+  const tempGroupedSquad = groupListByProp<SquadEntity>('role', squad)
+  if (tempGroupedSquad.has(BOWL_ALLROUNDER) || tempGroupedSquad.has(BAT_ALLROUNDER)) {
+    const battingAllRounder = tempGroupedSquad.get(BAT_ALLROUNDER) || []
+    const bowlingAllRounder = tempGroupedSquad.get(BOWL_ALLROUNDER) || []
+    tempGroupedSquad.delete(BOWL_ALLROUNDER)
+    tempGroupedSquad.delete(BAT_ALLROUNDER)
+    tempGroupedSquad.set(ALLROUNDER, [...battingAllRounder, ...bowlingAllRounder])
+  } else if (tempGroupedSquad.has(BAT_WK) || tempGroupedSquad.has(BOWL_WK)) {
+    const battingWk = tempGroupedSquad.get(BAT_WK) || []
+    const bowlingWk = tempGroupedSquad.get(BOWL_WK) || []
+    tempGroupedSquad.delete(BAT_WK)
+    tempGroupedSquad.delete(BOWL_WK)
+    tempGroupedSquad.set(WK, [...battingWk, ...bowlingWk])
+  }
   return tempGroupedSquad
 }
 
