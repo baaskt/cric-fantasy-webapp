57fc9e0149d7118fd60f6b0807ace43c9f84e446
Abiranjan <abiranjan.p@gmail.com>
2024-03-25 09:35:51 +0100

    CF-62 retry logic added for http requests

--- src/app/hooks/useRequest.ts
+++ src/app/hooks/useRequest.ts
@@ -9,6 +9,15 @@ export function useRequest(url: string, noCache?: boolean) {
       revalidateOnMount: noCache ? true : undefined,
       revalidateOnFocus: noCache ? false : false,
       revalidateIfStale: noCache ? false : false,
+      onErrorRetry: (error, key, config, revalidate, { retryCount }) => {
+        // Only retry up to 2 times.
+        if (retryCount >= 2) return
+
+        // Retry after 5 seconds.
+        setTimeout(() => {
+          void revalidate({ retryCount })
+        }, 5000)
+      },
     },
   )
 
