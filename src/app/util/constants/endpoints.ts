901885b3aab5c8bc60dcd94d56acf1536ef9ea40
Abiranjan <abiranjan.p@gmail.com>
2024-05-09 23:48:44 +0200

    Update endpoints.ts

--- src/app/util/constants/endpoints.ts
+++ src/app/util/constants/endpoints.ts
@@ -1,5 +1,5 @@
-// export const API_URL = 'https://cric-fantasy-backend.onrender.com/api/'
-export const API_URL = 'https://cric-fantasy-backend-test.onrender.com/api/'
+export const API_URL = 'https://cric-fantasy-backend.onrender.com/api/'
+// export const API_URL = 'https://cric-fantasy-backend-test.onrender.com/api/'
 
 export const HEALTH_URL = 'health'
 export const USERS = {
