1fb0a173e21f076a049f0dc5936b7c306e9dd91a
Abiranjan <abiranjan.p@gmail.com>
2025-03-08 01:31:50 +0100

    CF-12 implement timer

--- src/app/components/auction/PlayerBidding.tsx
+++ src/app/components/auction/PlayerBidding.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import SecondHighestBidder from './SecondHighestBidder'
 import HighestBidder from './HighestBidder'
 import PlayerAuctionCard from './PlayerAuctionCard'
+import AuctionTimer from './AuctionTimer'
 
 function PlayerBidding() {
   return (
@@ -12,8 +13,9 @@ function PlayerBidding() {
       <div className='flex flex-row p-5 rounded-lg shadow-lg gap-10 w-fit'>
         <PlayerAuctionCard />
       </div>
-      <div className='flex items-center basis-1/3 justify-center'>
+      <div className='flex flex-col gap-4 items-center basis-1/3 justify-center'>
         <HighestBidder />
+        <AuctionTimer />
       </div>
     </div>
   )
